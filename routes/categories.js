e2fcfe9040ce3ae4f7211cd53280da68b3a94f23
Eyob1984 <tamireyob@gmail.com>
2020-04-11 12:04:38 -0700

    update and delete working

--- routes/categories.js
+++ routes/categories.js
@@ -6,20 +6,64 @@ const catSchema = require('../lib/models/categories-schema.js');
 const Model = require('../lib/models/model.js');
 
 const CatModel = new Model(catSchema);
-router.get('/', (req, res, next) => {
+
+/**
+ * Categories GET route
+ * @group Categories
+ * @route GET /categories
+ * @returns {object} 200 -This route show data from categories object
+ */
+router.get('/api/v1/products', (req, res, next) => {
   res.send('Hello form the the route')
 });
 
-router.get('/', async(req, res, next) => {
+/**
+ * Categories GET route
+ * @group Categories
+ * @route GET /categories
+ * @returns {object} 200 -This route show data from categories object
+ */
+
+router.get('/api/v1/categories', async(req, res, next) => {
   let results = await CatModel.readByQuery({});
   console.log('results', results)
-  res.send(results)
-})
+  res.send(results);
+});
 
-router.post('/categories', async(req, res, next) => {
+/**
+ * Categories POST route
+ * @group Categories
+ * @route POST /categories
+ * @returns {object} 200 -This route create data inside categories object
+ */
+
+router.post('/api/v1/categories', async(req, res, next) => {
   let record = await CatModel.create(req.body);
-  res.send(record)
-})
+  res.send(record);
+});
+
+/**
+ * Categories PUT route
+ * @group Categories
+ * @route PUT /categories/:id
+ * @returns {object} 200 -This route update data in categories object
+ */
+
+router.put('/api/v1/categories/:_id', async (req, res, next) => {
+  let record = await CatModel.update(req.params._id);
+  res.send(record);
+});
+
+/**
+ * Categories DELETE route
+ * @group Categories
+ * @route DELETE /categories/:id
+ * @returns {object} 200 -This route delete data in categories object
+ */
+
+router.delete('/api/v1/categories/:_id', async (req, res, next) => {
+  let record = await CatModel.delete (req.params._id)
+});
 
 
 
--- routes/products.js
+++ routes/products.js
@@ -6,22 +6,68 @@ const proSchema = require('../lib/models/products-schema.js');
 const Model = require('../lib/models/model.js');
 
 const ProModel = new Model(proSchema);
-console.log('PorModel', ProModel)
 
-router.get('/', async (req, res, next) => {
+/**
+ * Products GET route
+ * @group Products
+ * @route GET /products
+ * @returns {object} 200 -This route show data from products object
+ */
+
+router.get('/api/v1/products', async (req, res, next) => {
   let results = await ProModel.readByQuery({});
   res.send(results)
 });
 
-router.get('/:_id', async (req, res, next) => {
+/**
+ * Products GET route
+ * @group Products
+ * @route GET /products
+ * @returns {object} 200 -This route show data from products object
+ */
+
+router.get('/api/v1/products/:_id', async (req, res, next) => {
   let record = await ProModel.read(req.params._id);
   res.send(record);
 });
 
-router.post('/', async (req, res, next) => {
+/**
+ * Products POST route
+ * @group Products
+ * @route POST /products
+ * @returns {object} 200 -This route create data inside products object
+ */
+
+router.post('/api/v1/products', async (req, res, next) => {
   let record = await ProModel.create(req.body)
 
+  res.send(record);
+});
+
+/**
+ * Products PUT route
+ * @group Products
+ * @route PUT /products/:id
+ * @returns {object} 201 -This route update data in products object
+ */
+
+router.put('/api/v1/products/:_id', async (req, res, next) => {
+  let record = await ProModel.update(req.params._id)
+  console.log('update', update)
+  res.send(record);
+});
+
+/**
+ * Products DELETE route
+ * @group Products
+ * @route DELETE /products/:id
+ * @returns {object} 200 -This route delete data in products object
+ */
+
+router.delete('/api/v1/products/:_id', async (req, res, next) => {
+  let record = await ProModel.delete(req.params._id);
+  console.log('delete in product routes', record);
   res.send(record)
-})
+});
  
 module.exports = router;
